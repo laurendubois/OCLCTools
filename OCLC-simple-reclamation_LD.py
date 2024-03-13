2171be1c1de426a9fa8ce85931b79b7b38cfda91
Lauren D <144151898+laurendubois@users.noreply.github.com>
2024-03-13 15:45:55 -0500

    Update OCLC-simple-reclamation_LD.py

--- OCLC-simple-reclamation_LD.py
+++ OCLC-simple-reclamation_LD.py
@@ -1,46 +1,34 @@
 import pandas as pd
 from pathlib import Path
 
-# Locate the files
-# downloads_path = Path("C:/Users/lmd8/OneDrive - Rice University/Desktop/testing")
+# Define input and output directories
+input_dir = Path("C:/Users/lmd8/OneDrive - Rice University/Desktop/testing/")
+output_dir = Path("C:/Users/lmd8/OneDrive - Rice University/Desktop/testing/")
 
 # Read the files
-# ALMAdf = pd.read_csv(downloads_path / "ART_v3.txt", sep='\t', dtype=str)
-# OCLCdf = pd.read_csv(downloads_path / "OCLC.txt", sep='\t', dtype=str)
-
-# Read the files
-ALMAdf = pd.read_csv("C:/Users/lmd8/OneDrive - Rice University/Desktop/testing/ART_testfile.txt", sep='\t')
-OCLCdf = pd.read_csv("C:/Users/lmd8/OneDrive - Rice University/Desktop/testing/OCLC.txt", sep='\t')
-
-
-# Print some values in the DataFrames to ensure data was read correctly
-print("ALMAdf head:")
-print(ALMAdf.head())
-print("OCLCdf head:")
-print(OCLCdf.head())
+try:
+    ALMAdf = pd.read_csv(input_dir / "ART-testfile-scrubbed.txt", sep='\t')
+    OCLCdf = pd.read_csv(input_dir / "OCLC-merged-original.txt", sep='\t')
+except FileNotFoundError as e:
+    print("Error: One or more input files not found.")
+    exit()  # Terminate script execution if files are not found
 
 # Generalized extraction of numeric patterns, treating each number as a string
 def extract_numeric(df, col_name):
-    # This function will extract all numeric sequences from the specified column and return them
+    # Extract all numeric sequences from the specified column and return them
     df[col_name] = df[col_name].str.extractall(r'(\d+)').groupby(level=0).apply(lambda x: '|'.join(x[0])).astype(str)
 
 # Apply the function to each relevant column in both DataFrames
 extract_numeric(ALMAdf, '035$a')
 extract_numeric(OCLCdf, '001')
 
-# find unmatched
+# Find unmatched
 NotInOCLC = ALMAdf[~ALMAdf['035$a'].isin(OCLCdf['001'])]
 NotInAlma = OCLCdf[~OCLCdf['001'].isin(ALMAdf['035$a'])]
 
 # Save results to TXT (TSV)
-NotInOCLC.to_csv("C:/Users/lmd8/OneDrive - Rice University/Desktop/testing/NotInOCLC.txt", sep='\t', index=False, columns=['001', '035$a'])
-NotInAlma.to_csv("C:/Users/lmd8/OneDrive - Rice University/Desktop/testing/NotInALMA.txt", sep='\t', index=False, columns=['001'])
-
-print("ALMAdf['035$a'] values:")
-print(ALMAdf['035$a'])
-
-print("OCLCdf['001'] values:")
-print(OCLCdf['001'])
+NotInOCLC.to_csv(output_dir / "NotInOCLC.txt", sep='\t', index=False, columns=['001', '035$a'])
+NotInAlma.to_csv(output_dir / "NotInALMA.txt", sep='\t', index=False, columns=['001'])
 
 # Print completion message
-print("Process complete. Check output file.")
+print("Finished. Check output files.")
